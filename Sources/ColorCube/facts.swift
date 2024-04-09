eee91d9bca2c7cd20886b3401cf117063bff23b2
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2024-04-09 10:55:02 +0100

    Update facts.swift

--- Sources/ColorCube/facts.swift
+++ Sources/ColorCube/facts.swift
@@ -8,11 +8,11 @@
 @_exported import ColorCubeObjc
 
 extension CCFlags: OptionSet {
-  static let onlyBrightColors = CCOnlyBrightColors
-  static let onlyDarkColors = CCOnlyDarkColors
-  static let onlyDistinctColors = CCOnlyDistinctColors
-  static let orderByBrightness = CCOrderByBrightness
-  static let orderByDarkness = CCOrderByDarkness
-  static let avoidWhite = CCAvoidWhite
-  static let avoidBlack = CCAvoidBlack
+  public static let onlyBrightColors = CCOnlyBrightColors
+  public static let onlyDarkColors = CCOnlyDarkColors
+  public static let onlyDistinctColors = CCOnlyDistinctColors
+  public static let orderByBrightness = CCOrderByBrightness
+  public static let orderByDarkness = CCOrderByDarkness
+  public static let avoidWhite = CCAvoidWhite
+  public static let avoidBlack = CCAvoidBlack
 }

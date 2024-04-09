19c8405a58eb9656652a6921bc9d5cf75c8135e1
Lakhan Lothiyi <llsc12.dev@gmail.com>
2024-04-09 09:41:06 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ColorCube",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ColorCube",
+            targets: ["ColorCube"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ColorCube"),
+        .testTarget(
+            name: "ColorCubeTests",
+            dependencies: ["ColorCube"]),
+    ]
+)
